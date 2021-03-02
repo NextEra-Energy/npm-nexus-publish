570bac7646da7d558e3238b122e1999527d48adb
Eugene Ryzhikov <eugene.ryzhikov@nee.com>
2021-03-02 17:58:22 -0500

    Check for npmrc

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 const core = require('@actions/core');
 // const github = require('@actions/github');
 const spawn  = require('child_process');
+const fs = require('fs')
 
 const base64encode = (b) => Buffer.from(b).toString('base64');
+const npmrc = "./.npmrc"
 
 
 async function run() {
@@ -11,11 +13,20 @@ async function run() {
         const user = core.getInput("nexus-user")
         const pswd = core.getInput("nexus-password")
 
+        if (fs.existsSync(npmrc)) {
+            core.info("Using existing .npmrc")
+        } else {
+            core.info("Creating .npmrc configuration ")
+            fs.writeFileSync(npmrc, '???')
+        }
+
+
         core.info("Authenticating to Nexus repository...")
         spawn.exec('npm set _auth ' + base64encode( `${user}:${pswd}`) )
 
         core.info("Publishing...")
         spawn.exec('npm publish')
+
 
         core.info("The module is successfully published!")
 
