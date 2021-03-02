6691efb2904849ecd943fa545f3dade508d64a38
Eugene Ryzhikov <eugene.ryzhikov@nee.com>
2021-03-02 13:42:16 -0500

    Switch to a better base64 function

--- index.js
+++ index.js
@@ -2,19 +2,24 @@ const core = require('@actions/core');
 // const github = require('@actions/github');
 const spawn  = require('child_process');
 
+const base64encode = (b) => Buffer.from(b).toString('base64');
+
+
 async function run() {
     try {
 
         const user = core.getInput("nexus-user")
         const pswd = core.getInput("nexus-password")
 
-        spawn.exec('npm set _auth ' + btoa( `${user}:${pswd}`) )
+        spawn.exec('npm set _auth ' + base64encode( `${user}:${pswd}`) )
         spawn.exec('npm publish')
 
     } catch (error) {
         core.setFailed(error.message);
     }
 }
+
+
 
 run();
 
